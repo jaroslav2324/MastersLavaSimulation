74167f3df6f31d5f7fd97a9162927e16f9ba733f
jaroslav2324 <jaro_r@mail.ru>
2025-09-10 20:38:42 +0300

    started

---
+++ main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment (lib,"D3D12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
---
+++ src/RenderSubsystem.cc
@@ -0,0 +1,2 @@
+#include "RenderSubsystem.h"
+
